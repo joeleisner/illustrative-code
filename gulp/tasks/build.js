0e25471553710ea40209eb5d3a045a4104866f65
Joel Eisner <jeisner93@gmail.com>
2019-04-07 12:11:37 -0400

    Updated the build task to include scopes for global/project assets

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -3,14 +3,30 @@ const gulp = require('gulp'),
     pug    = require('./pug'),
     sass   = require('./sass');
 
-function build(style) {
-    const index = style || 'all';
-    return gulp.series(gulp.parallel(js[index], pug.global, pug.projects, sass.global[index], sass.projects[index]));
+function build(context, style) {
+    const index = style || 'all',
+        tasks   = [ pug[context], sass[context][index] ];
+
+    if ([ 'all', 'global' ].includes(context)) tasks.unshift(js[index]);
+
+    return gulp.series(gulp.parallel(...tasks));
 }
 
 module.exports = {
-    all: build(),
-    exp: build('exp'),
-    min: build('min'),
+    all: {
+        all: build('all'),
+        exp: build('all', 'exp'),
+        min: build('all', 'min')
+    },
+    global: {
+        all: build('global'),
+        exp: build('global', 'exp'),
+        min: build('global', 'min')
+    },
+    projects: {
+        all: build('projects'),
+        exp: build('projects', 'exp'),
+        min: build('projects', 'min')
+    },
     ref: build
 };
--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,35 @@ const build = require('./gulp/tasks/build'),
     sass    = require('./gulp/tasks/sass'),
     watch   = require('./gulp/tasks/watch');
 
-// Build -----------------------------------------
-gulp.task('build',                     build.all);
-gulp.task('build:exp',                 build.exp);
-gulp.task('build:min',                 build.min);
-// Default ---------------------------------------
-gulp.task('default',                       watch);
-// JS --------------------------------------------
-gulp.task('js',                           js.all);
-gulp.task('js:exp',                       js.exp);
-gulp.task('js:min',                       js.min);
-// PUG -------------------------------------------
-gulp.task('pug',                         pug.all);
-gulp.task('pug:global',               pug.global);
-gulp.task('pug:projects',           pug.projects);
-// SASS ------------------------------------------
-gulp.task('sass',                   sass.all.all);
-gulp.task('sass:exp',               sass.all.exp);
-gulp.task('sass:min',               sass.all.min);
-gulp.task('sass:global',         sass.global.all);
-gulp.task('sass:global:exp',     sass.global.exp);
-gulp.task('sass:global:min',     sass.global.min);
-gulp.task('sass:projects',     sass.projects.all);
-gulp.task('sass:projects:exp', sass.projects.exp);
-gulp.task('sass:projects:min', sass.projects.min);
-// Watch -----------------------------------------
-gulp.watch('watch',                        watch);
+// Build -------------------------------------------
+gulp.task('build',                   build.all.all);
+gulp.task('build:exp',               build.all.exp);
+gulp.task('build:min',               build.all.min);
+gulp.task('build:global',         build.global.all);
+gulp.task('build:global:exp',     build.global.exp);
+gulp.task('build:global:min',     build.global.min);
+gulp.task('build:projects',     build.projects.all);
+gulp.task('build:projects:exp', build.projects.exp);
+gulp.task('build:projects:min', build.projects.min);
+// Default -----------------------------------------
+gulp.task('default',                         watch);
+// JS ----------------------------------------------
+gulp.task('js',                             js.all);
+gulp.task('js:exp',                         js.exp);
+gulp.task('js:min',                         js.min);
+// PUG ---------------------------------------------
+gulp.task('pug',                           pug.all);
+gulp.task('pug:global',                 pug.global);
+gulp.task('pug:projects',             pug.projects);
+// SASS --------------------------------------------
+gulp.task('sass',                     sass.all.all);
+gulp.task('sass:exp',                 sass.all.exp);
+gulp.task('sass:min',                 sass.all.min);
+gulp.task('sass:global',           sass.global.all);
+gulp.task('sass:global:exp',       sass.global.exp);
+gulp.task('sass:global:min',       sass.global.min);
+gulp.task('sass:projects',       sass.projects.all);
+gulp.task('sass:projects:exp',   sass.projects.exp);
+gulp.task('sass:projects:min',   sass.projects.min);
+// Watch -------------------------------------------
+gulp.watch('watch',                          watch);
