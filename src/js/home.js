2a1b28c5aee80dae021abb75c9dfcdd4fbd305ee
Joel Eisner <jeisner93@gmail.com>
2020-11-09 20:34:34 -0500

    JS optimizations and changes
    Updated the home and project JS to clean up the code, use modern syntax,
    and simplify everything.
    
    This change also introduced changes to the way sections are
    hidden/shown. Previously, it used a `.hidden` and `.visible` class to
    accomplish this. Now, it sections are hidden by default, and made
    visible when their `aria-hidden` attribute is set to false.

--- src/js/home.js
+++ src/js/home.js
@@ -1,75 +1,100 @@
-// Add image lazy-loading to cards
-function cards() {
-    // Select all cards
-    let wrapper = document.getElementById('wrapper'),
-        cards =   document.querySelectorAll('.card'),
-        i;
-    // Element is in viewport function
-    function elementIsInViewport(element) {
-        // Get the bounding client rect of element
-        var rect = element.getBoundingClientRect();
-        // Return true if the element is within the viewport, and false if it is not
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.top <= (window.innerHeight || document.documentElement.clientHeight)
-        );
-    }
-    // Process cards
-    function processCards() {
-        // For each card...
-        for (i = 0; i < cards.length; i++) {
-            let card = cards[i];
-            // ..., if it's within the viewport, ...
-            if (elementIsInViewport(card)) {
-                // ... load its image
-                loadImage(card);
-            }
-        }
-    }
-    // Load image function
-    function loadImage(card) {
-        // Get the data-src attribute of the card's image and create a new, virtual image
-        let wrap =      card.querySelector('.wrap'),
-            image =     card.getElementsByTagName('img')[0],
-            dataSrc =   image.getAttribute('data-src'),
-            lazyImage = new Image();
-        image.classList.remove('loading');
-        // If there's no data-src attribute on the card's image, it's been loaded
-        if (!dataSrc) return;
-        // When the virtual image loades...
-        lazyImage.onload = function() {
-            // ... set the passed image's source to the virtual image's source,...
-            image.src = this.src;
-            // ... remove the data-src attribute from the image,...
-            image.removeAttribute('data-src');
-            // ... and add a "loaded" class to the card
-            wrap.classList.remove('loading');
-        };
-        // Set the virtual image's source to the passed image's data-src
-        lazyImage.src = dataSrc;
-    }
-    // Process the cards when initialized...
-    processCards();
-    // ... and when the window is scrolled
-    wrapper.addEventListener('scroll', processCards);
+// Returns whether an element is within the viewport or not
+function elementIsInViewport(element) {
+    // Get the bounding client rect of element...
+    var rect = element.getBoundingClientRect();
+    // ... and return whether it's within the viewport or not
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+    );
 }
 
-function navShadow() {
-    let nav = document.getElementById('nav'),
-        wrapper = document.getElementById('wrapper');
+// Load image function
+function lazyLoadElementImage(element) {
+    // Grab the element's internal wrapper...
+    const wrap = element.querySelector('.wrap');
+    // ... and image
+    const image = element.querySelector('img');
 
-    function addShadow() {
-        nav.className = wrapper.scrollTop ? 'shadow' : '';
-    }
+    // If the internal wrapper or image do exist, do nothing
+    if (!wrap || !image) return;
 
-    wrapper.addEventListener('scroll', addShadow);
+    // Next, grab the proper source from the element's dataset...
+    const { src } = image.dataset;
+    // ... and create a new lazy image
+    const lazyImage = new Image;
+
+    // Remove the image's loading class
+    image.classList.remove('loading');
+
+    // If no proper source was defined in the element's dataset, do nothing else
+    if (!src) return;
+
+    // When the lazy image loads,...
+    lazyImage.onload = function() {
+        // ... set the actual image's source to that of the lazy image,...
+        image.src = this.src;
+        // ... remove the data-src attribute from the actual image,...
+        image.removeAttribute('data-src');
+        // ... and remove the wrapper's loading class
+        wrap.classList.remove('loading');
+    };
+
+    // Set the lazy image's source to that of the actual image's proper source
+    lazyImage.src = src;
 }
 
-// Initialize the contained functions
+// Lazy loads an element's image if within the viewport
+function lazyLoadElementImageIfInViewport(element) {
+    if (elementIsInViewport(element)) return lazyLoadElementImage(element);
+}
+
+// Lazy load card images when within the viewport
+function lazyLoadCardImages() {
+    // Grab the wrapper...
+    const wrapper = document.getElementById('wrapper');
+    // ... and all cards
+    const cards = Array.from(document.querySelectorAll('.card'));
+
+    // If no wrapper or cards exist, do nothing
+    if (!wrapper || !cards.length) return;
+
+    // Process card images
+    function processCardImages() {
+        cards.map(lazyLoadElementImageIfInViewport);
+    }
+
+    // Process each card's image when initialized...
+    processCardImages();
+    // ... and whenever the window is scrolled thereafter
+    wrapper.addEventListener('scroll', processCardImages);
+}
+
+// Toggles the shadow class on a given element
+function toggleShadowClass() {
+    // Give the element the shadow class if the scroll position isn't at the top
+    this.className = wrapper.scrollTop ? 'shadow' : '';
+}
+
+// Toggles the nav's drop-shadow based on the scroll position
+function toggleNavShadow() {
+    // Grab the nav...
+    const nav = document.getElementById('nav');
+    // ... and wrapper element
+    const wrapper = document.getElementById('wrapper');
+
+    // If the nav or wrapper do not exist, do nothing
+    if (!nav || !wrapper) return;
+
+    // Finally, toggle the nav's shadow class based on the wrapper scroll position
+    wrapper.addEventListener('scroll', toggleShadowClass.bind(nav));
+}
+
+// Initialize the homaepgae functionality
 function initialize() {
-    cards();
-    navShadow();
+    lazyLoadCardImages();
+    toggleNavShadow();
 }
 
 // When the DOM is loaded, initialize all JS
