a7682ebb5bc2e6c2e391aaab39b81b9004b8064a
Joel Eisner <jeisner93@gmail.com>
2021-10-02 21:46:25 -0400

    Footer component fix
    Fixed the footer component's HTML output to ensure a space is included
    after the heart icon.

--- components/footer/html.js
+++ components/footer/html.js
@@ -4,7 +4,7 @@ export function Footer() {
     return html`
         <footer class="footer">
             <div class="footer__container">
-                <p>This site was made with ${ Icon({ icon: 'heart', text: 'love' }) } by Joel Eisner and is <a href="https://www.w3.org/TR/WCAG21/" rel="noopener noreferrer" target="_blank">WCAG 2.1 AAA</a> accessibile.</p>
+                <p>This site was made with ${ Icon({ icon: 'heart', text: 'love' }) }  by Joel Eisner and is <a href="https://www.w3.org/TR/WCAG21/" rel="noopener noreferrer" target="_blank">WCAG 2.1 AAA</a> accessibile.</p>
             </div>
         </footer>
     `;
