d79553204632e2604a4b7aeffc7da641250b4d2a
Joel Eisner <jeisner93@gmail.com>
2022-02-14 17:52:16 -0500

    Service worker changes
    Updated the `service-worker.js` file to breakout/name anonymous
    functions for better maintenance. This includes the `preCache()`,
    `cleanup(currentCaches)`, and `useCache(event)` functions.

--- service-worker.js
+++ service-worker.js
@@ -19,35 +19,62 @@ const PRECACHE_URLS = [
     'site.js'
 ];
 
+// Caches always needed assets
+async function preCache() {
+    const cache = await caches.open(CACHES.PRE);
+    await cache.addAll(PRECACHE_URLS);
+}
+
 // Install handler (precache assets)
 self.addEventListener('install', event => {
-    event.waitUntil((async () => {
-        const cache = await caches.open(CACHES.PRE);
-
-        await cache.addAll(PRECACHE_URLS);
-    })());
-
+    event.waitUntil(preCache());
     self.skipWaiting();
 });
 
-// Activate handler (clean up old cached)
+// Clean up old caches
+async function cleanup(currentCaches) {
+    const cacheNames = await caches.keys();
+
+    const cachesToDelete = cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+
+    await Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+    }));
+}
+
+// Activate handler (clean up old caches)
 self.addEventListener('activate', event => {
     const currentCaches = Object.values(CACHES);
-
-    event.waitUntil((async () => {
-        const cacheNames = await caches.keys();
-
-        const cachesToDelete = cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-
-        await Promise.all(cachesToDelete.map(cacheToDelete => {
-            return caches.delete(cacheToDelete);
-        }));
-    })());
-
+    event.waitUntil(cleanup(currentCaches));
     self.clients.claim();
 });
 
-// Fetch handler (use cache for local assets if possible)
+// Use caches assets if possible
+async function useCache(event) {
+    try {
+        const cachedResponse = await caches.match(event.request);
+
+        if (cachedResponse) return cachedResponse;
+
+        const cache = await caches.open(CACHES.RUN);
+
+        const response = await fetch(event.request);
+
+        await cache.put(event.request, response.clone());
+
+        return response;
+    } catch (error) {
+        if (event.request.mode === 'navigate') {
+            console.error('Fetch failed; returning offline page instead.', error);
+
+            const cache = await caches.open(CACHES.PRE);
+            const offlinePath = await cache.match(OFFLINE_ASSETS.html);
+            return offlinePath;
+        }
+    }
+}
+
+// Fetch handler (use cached assets if possible)
 self.addEventListener('fetch', event => {
     // Ignore cross-origin requests
     if (!event.request.url.startsWith(self.location.origin)) return;
@@ -55,27 +82,5 @@ self.addEventListener('fetch', event => {
     // Ignore BrowserSync requests
     if (event.request.url.includes('browser-sync')) return;
 
-    event.respondWith((async () => {
-        try {
-            const cachedResponse = await caches.match(event.request);
-
-            if (cachedResponse) return cachedResponse;
-
-            const cache = await caches.open(CACHES.RUN);
-
-            const response = await fetch(event.request);
-
-            await cache.put(event.request, response.clone());
-
-            return response;
-        } catch (error) {
-            if (event.request.mode === 'navigate') {
-                console.error('Fetch failed; returning offline page instead.', error);
-
-                const cache = await caches.open(CACHES.PRE);
-                const offlinePath = await cache.match(OFFLINE_ASSETS.html);
-                return offlinePath;
-            }
-        }
-    })());
+    event.respondWith(useCache(event));
 });
